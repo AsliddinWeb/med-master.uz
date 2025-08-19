406a6c94f2fe3291668c4f49fa0899de8e0d2c06
AsliddinWeb <asliddinsinger@gmail.com>
2025-08-19 16:39:27 +0500

    base.py updated

--- config/settings/base.py
+++ config/settings/base.py
@@ -3,6 +3,8 @@ from decouple import config
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
+ALLOWED_HOSTS = ['*']
+
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-fallback-key')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
