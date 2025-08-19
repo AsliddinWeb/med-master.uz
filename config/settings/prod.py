21e5a48fdbbf34cbe891209f4cf4c76f414c38d3
AsliddinWeb <asliddinsinger@gmail.com>
2025-08-19 16:07:25 +0500

    settings.py updated

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,7 +1,7 @@
 from .base import *
 import os
 
-ALLOWED_HOSTS = ['med-master.uz', 'www.med-master.uz']
+ALLOWED_HOSTS = ['med-master.uz', 'www.med-master.uz', '167.71.49.146', 'localhost']
 
 # Database (production uchun PostgreSQL)
 DATABASES = {
